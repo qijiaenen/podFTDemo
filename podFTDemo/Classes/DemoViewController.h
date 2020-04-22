64e1ec7c93e6487740d1238c5faa8fc72a2110e9
smileZhuo <120916239@qq.com>
2020-04-22 15:13:56 +0800

    提交

---
+++ podFTDemo/Classes/DemoViewController.h
@@ -0,0 +1,17 @@
+//
+//  DemoViewController.h
+//  FrameworkDemo
+//
+//  Created by 卓鑫钊 on 2020/4/17.
+//  Copyright © 2020 Samil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DemoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ podFTDemo/Classes/DemoViewController.m
@@ -0,0 +1,24 @@
+//
+//  DemoViewController.m
+//  FrameworkDemo
+//
+//  Created by 卓鑫钊 on 2020/4/17.
+//  Copyright © 2020 Samil. All rights reserved.
+//
+
+#import "DemoViewController.h"
+
+@interface DemoViewController ()
+
+@end
+
+@implementation DemoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+
+@end
